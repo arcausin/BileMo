6c68dfd1fc1b6d4567807a8986903d90d4538158
arcausin <arcausin@gmail.com>
2023-10-12 10:02:24 +0200

    Make crud phones

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -2,12 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Phone;
 use App\Repository\PhoneRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 class PhoneController extends AbstractController
 {
@@ -20,6 +25,22 @@ class PhoneController extends AbstractController
         return new JsonResponse($jsonPhones, Response::HTTP_OK, [], true);
     }
 
+    #[Route('/api/phones', name: 'app_phones_create', methods: ['POST'])]
+    public function create(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, UrlGeneratorInterface $urlGenerator): JsonResponse
+    {
+        $phone = $serializer->deserialize($request->getContent(), Phone::class, 'json');
+        $content = $request->toArray();
+        $phone->setReleaseAt(new \DateTimeImmutable($content['releaseAt']));
+        $em->persist($phone);
+        $em->flush();
+
+        $jsonPhone = $serializer->serialize($phone, 'json');
+
+        $location = $urlGenerator->generate('app_phones_show', ['id' => $phone->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($jsonPhone, Response::HTTP_CREATED, ['Location' => $location], true);
+    }
+
     #[Route('/api/phones/{id}', name: 'app_phones_show', methods: ['GET'])]
     public function show(int $id, PhoneRepository $phoneRepository, SerializerInterface $serializer): JsonResponse
     {
@@ -28,6 +49,37 @@ class PhoneController extends AbstractController
         if ($phone) {
             $jsonPhone = $serializer->serialize($phone, 'json');
             return new JsonResponse($jsonPhone, Response::HTTP_OK, [], true);
+        }
+
+        return new JsonResponse('Phone not found', Response::HTTP_NOT_FOUND);
+    }
+
+    #[Route('/api/phones/{id}', name: 'app_phones_update', methods: ['PUT'])]
+    public function update(int $id, Request $request, PhoneRepository $phoneRepository, SerializerInterface $serializer, EntityManagerInterface $em): JsonResponse
+    {
+        $phone = $phoneRepository->find($id);
+
+        if ($phone) {
+            $updatePhone = $serializer->deserialize($request->getContent(), Phone::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $phone]);
+            $em->persist($updatePhone);
+            $em->flush();
+
+            return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+        }
+
+        return new JsonResponse('Phone not found', Response::HTTP_NOT_FOUND);
+    }
+
+    #[Route('/api/phones/{id}', name: 'app_phones_delete', methods: ['DELETE'])]
+    public function delete(int $id, PhoneRepository $phoneRepository, EntityManagerInterface $em): JsonResponse
+    {
+        $phone = $phoneRepository->find($id);
+
+        if ($phone) {
+            $em->remove($phone);
+            $em->flush();
+
+            return new JsonResponse(null, Response::HTTP_NO_CONTENT);
         }
 
         return new JsonResponse('Phone not found', Response::HTTP_NOT_FOUND);
